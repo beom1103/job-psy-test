4ae7016285000bfac8aee9abf6d183085edbf439
3기-1반 애범 <gogot1103@naver.com>
2021-11-16 20:09:56 +0900

    User정보창 기본 설계

--- project/src/components/User.js
+++ project/src/components/User.js
@@ -8,8 +8,19 @@ function User({ onInsert }) {
       </h1>
 
       <form>
-        이름 : <input type="text" placeholder="이름을 입력하세요." />
-        <br />
+        <div>
+          이름 : <input type="text" placeholder="이름을 입력하세요." />
+        </div>
+        {/* 한개만 체크할 수 있도록 구현 */}
+        <div>
+          <input type="checkbox" name="남자" />
+          <label for="남자">남자</label>
+          <input type="checkbox" name="여자" />
+          <label for="여자">여자</label>
+        </div>
+
+        
+        
         <button>검사시작</button>
         
       </form>
