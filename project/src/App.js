114688900422c11a240150fa75727bc4b11362b8
3기-1반 애범 <gogot1103@naver.com>
2021-11-17 16:42:53 +0900

    User(component) 유효성 검사 및 오류메세지 구현

--- project/src/App.js
+++ project/src/App.js
@@ -1,10 +1,20 @@
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import User from "./components/User";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <User />
+      <BrowserRouter>
+        
+          <User />
+        
+
+
+      </BrowserRouter>
+
+      
 
     </div>
   );
--- project/src/components/User.js
+++ project/src/components/User.js
@@ -1,6 +1,40 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 
 function User({ onInsert }) {
+
+  // 유효성 검사
+  const [isName, setIsName] = useState(false);
+  const [isChecked, setIsChecked] = useState(false);
+  // 이름을 입력받는 state
+  const [userName, setUserName] = useState("");
+
+  // 오류메세지
+  const [nameMessage, setNameMessage] = useState('')
+  const [checkMessage, setCheckMessage] = useState('성별을 체크해주세요.')
+
+
+  const onChangeName = useCallback((e) => {
+    setUserName(e.target.value)
+    if (userName.length === 0) {
+      setNameMessage('')
+      setIsName(false)
+    } else if (userName.length < 2 || userName.length > 5) {
+      setNameMessage('2글자 이상 5글자 미만으로 입력해주세요.')
+      setIsName(false)
+    } else {
+      setNameMessage('올바른 이름 형식입니다')
+      setIsName(true)
+    }
+  }, [userName])
+
+  
+  const onChangeChecked = () => {
+    setIsChecked(true)
+    if (checkMessage === true) {
+      setCheckMessage("")
+    }
+  }
+
   return (
     <div>
       <h1>
@@ -9,19 +43,33 @@ function User({ onInsert }) {
 
       <form>
         <div>
-          이름 : <input type="text" placeholder="이름을 입력하세요." />
+          <label for="이름">이름 : </label>
+          <input 
+            type="text"
+            value = {userName} 
+            name="이름" 
+            placeholder="이름을 입력하세요." 
+            onChange={onChangeName} />
+    
+          <p>{nameMessage}</p>
         </div>
-        {/* 한개만 체크할 수 있도록 구현 */}
-        <div>
-          <input type="checkbox" name="남자" />
-          <label for="남자">남자</label>
-          <input type="checkbox" name="여자" />
-          <label for="여자">여자</label>
+        <br />
+
+        <div className="checkBox" onChange={onChangeChecked}>
+          <span>
+            <label for="gender">성별 : </label>
+            <input type="radio" value="M" name="gender" id="male" />남
+            <input type="radio" value="F" name="gender" id="female" />여
+          </ span>  &nbsp;
+          <span>{checkMessage}</span>
         </div>
 
-        
-        
-        <button>검사시작</button>
+        <br />
+        {/* 재사용 컴포넌트로 만들어야함. */}
+        <button 
+          type="submit"
+          disabled={!(isName && isChecked)}
+        >검사시작</button>
         
       </form>
     </div>
