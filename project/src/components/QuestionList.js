7136697a08dc45683b164add3fc58abeca96001d
3기-1반 애범 <gogot1103@naver.com>
2021-11-18 19:25:46 +0900

    API로  모든 문항 출력

--- project/src/components/QuestionList.js
+++ project/src/components/QuestionList.js
@@ -28,12 +28,17 @@ const QuestionList = () => {
         {question.map((question)=> {
           return (
             <div>
-              <p>
-                {question['question']}
-              </p>
+              <h3>
+                {question['qitemNo']}번 {question['question']}
+              </h3>
               <form>
-                <input type="radio" name="answer" />
-                <input type="radio" name="answer" />
+                <input type="radio" name="answer" value={question['answerScore01']}/>
+                <span name="1">{question['answer01']} : </span> 
+                <label htmlFor="1">{question['answer03']}</label>
+                <br />
+                <input type="radio" name="answer" value={question['answerScore02']} />
+                <span name="2">{question['answer02']} : </span>
+                <label htmlFor="2">{question['answer04']}</label>
               </form>
             </div>
           )
