a7af638666a041f78d44fdc14db2f8b5addf57c2
3기-1반 애범 <gogot1103@naver.com>
2021-11-19 13:34:03 +0900

    TestPage에서 prop을 전달하는 방식으로 바꾸고 페이지를  슬라이싱함.

--- project/src/components/QuestionList.js
+++ project/src/components/QuestionList.js
@@ -1,28 +1,8 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios';
+import React from 'react'
 
 
-const QuestionList = () => {
-  const [question, setQusetion] = useState([]);
-  const key = '5fcc4366025782ac126088744b9620ea';
+const QuestionList = ({question}) => {
   
-
-
-
-  useEffect(() => {
-    async function fetchQuestions() {
-      try {
-        const res = await axios.get(`https://www.career.go.kr/inspct/openapi/test/questions?apikey=${key}&q=6`)
-        setQusetion(res.data["RESULT"]);
-        console.log(question)
-      } catch (e) {
-        console.log('Error!')
-      }
-    }
-
-    fetchQuestions();
-
-  }, [])
   return (
     <div>
         {question.map((question) => {
--- project/src/pages/TestPage.js
+++ project/src/pages/TestPage.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import QuestionList from '../components/QuestionList';
-
-
+import axios from 'axios';
 
 const TestPage = () => {
+  const [question, setQusetion] = useState([]);
+  const key = '5fcc4366025782ac126088744b9620ea';
+  
+  // 현재 페이지
+  const [curPage, setCurPage] = useState(1);
+  // 페이지 당 문항수
+  const [perPage, setPerPage] = useState(5);
+
+
+  // 해당 페이지의 마지막 index 번호
+  const last = curPage * perPage;  
+
+  // 해당 페이지의 첫번째 index 번호
+  const first = last - perPage;
+
+  // 각 페이지에서 보여질 배열
+  const cur = question.slice(first, last);
+
+  // API 요청
+  useEffect(() => {
+    async function fetchQuestions() {
+      try {
+        const res = await axios.get(`https://www.career.go.kr/inspct/openapi/test/questions?apikey=${key}&q=6`)
+        setQusetion(res.data["RESULT"]);
+        console.log(question)
+      } catch (e) {
+        console.log('Error!')
+      }
+    }
+
+    fetchQuestions();
+
+  }, [])
 
   return (
     <div>
@@ -12,9 +44,7 @@ const TestPage = () => {
         {/*여기에 진행 막대기 넣어야함.*/}
       </div>
       
-      <QuestionList>
-
-      </QuestionList>
+      <QuestionList question={cur}  />
       
     </div>
   )
