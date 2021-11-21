6c53d641e0a2e5f41a24c44847d436034b36c43f
3기-1반 애범 <gogot1103@naver.com>
2021-11-22 01:38:21 +0900

    드디어 localstorage를 이용해 check값을 유지시켰다...ㅠㅠ 3일 걸렸다...

--- project/src/components/QuestionList.js
+++ project/src/components/QuestionList.js
@@ -5,20 +5,37 @@ import { UserContext } from "../components/UserStore";
 const QuestionList = ({question}) => {
   
   const [post,setPost] = useContext(UserContext);
+
+
+  // check값을 닮을 state
   const [inputs, setInputs] = useState([]);
-  
-  
+  // const [checkList, setCheckList] = useState([]);
   const objHandler =((e) => {
     const {name, value} = e.target;
-    
     setInputs((i) => {
       const newInputs = {...i};
       newInputs[name] = value
       return newInputs;
     })
 
+    localStorage.setItem(name, value)
   });
+  
+  const checked = question['answerS']
+  // function checked (e) {
+  //   setCheckList((i) =>{
+  //     const newCheckList =[e.target.value, ...checkList]
+  //     return newCheckList
+  //   })
 
+  //   for (let i in checkList) {
+  //     if (checkList[i] === e.target.value) {setCheck1(true)} else {setCheck2(true)}
+  //   } 
+  //   console.log(checkList)
+  // }
+  
+    
+  // 객체를 배열로 변환해서 post에 알맞은 형태로 변환해주는 함수
   useEffect(() => {
     const answerList = [];
     for (let i of Object.entries(inputs)) {
@@ -27,8 +44,10 @@ const QuestionList = ({question}) => {
     
       post.answers = answerList.join(" ");
       console.log(post)
-      
+      console.log(inputs)      
+  
   }, [inputs, post])
+
 
   return (
     <div>
@@ -43,8 +62,7 @@ const QuestionList = ({question}) => {
                   type="radio" 
                   name={"B"+question['qitemNo']} 
                   value={question['answerScore01']}
-                  
-
+                  checked={localStorage.getItem("B"+question['qitemNo']) === question['answerScore01']}
                   />
                 <span name="1">{question['answer01']} : </span> 
                 <label htmlFor="1">{question['answer03']}</label>
@@ -54,8 +72,9 @@ const QuestionList = ({question}) => {
                 <input 
                   type="radio" 
                   name={"B"+question['qitemNo']} 
-                  value={question['answerScore02']} 
-                  
+                  value={question['answerScore02']}
+                  checked={localStorage.getItem("B"+question['qitemNo']) === question['answerScore02']}
+
                   />
                 <span name="2">{question['answer02']} : </span>
                 <label htmlFor="2">{question['answer04']}</label>
